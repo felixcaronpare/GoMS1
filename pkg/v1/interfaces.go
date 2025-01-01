3175e333abd12b98cf42b1ab5b98fa8ad8e7bd63
felixcp <145021906+felixcaronpare@users.noreply.github.com>
2025-01-01 16:17:30 -0500

    interface fixes

--- pkg/v1/interfaces.go
+++ pkg/v1/interfaces.go
@@ -3,9 +3,12 @@ package v1
 import "github.com/felixcaronpare/GoMS1/internal/models"
 
 type RepoInterface interface {
-	Create(models.Account) error
+
+	Create(models.Account) (models.Account, error)
+
+	GetByEmail(email string) (models.Account, error)
 }
 
 type UsecaseInterface interface {
-	Create(models.Account) error
+	Create(models.Account) (models.Account, error)
 }

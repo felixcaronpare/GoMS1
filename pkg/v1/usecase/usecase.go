f8b7959573a2ed73e5a482cdf4d24673b5ab4dd1
felixcp <145021906+felixcaronpare@users.noreply.github.com>
2025-01-02 08:59:32 -0500

    gitconfig and gitignore

--- pkg/v1/usecase/usecase.go
+++ pkg/v1/usecase/usecase.go
@@ -5,7 +5,7 @@ import (
 
 	"github.com/felixcaronpare/GoMS1/internals/models"
 	interfaces "github.com/felixcaronpare/GoMS1/pkg/v1"
-	accountRepo "github.com/felixcaronpare/GomS1/pkg/v1/repository"
+	accountRepo "github.com/felixcaronpare/GoMS1/pkg/v1/repository"
 	"gorm.io/gorm"
 )
 

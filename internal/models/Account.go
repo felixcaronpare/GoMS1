a22a1cba1b39f80229cf614290782ddee58fa654
felixcp <145021906+felixcaronpare@users.noreply.github.com>
2025-01-08 11:30:32 -0500

    DB exposed and usable

--- internal/models/Account.go
+++ internal/models/Account.go
@@ -4,7 +4,7 @@ import "gorm.io/gorm"
 
 type Account struct {
 	gorm.Model
-	Name	 string
-	Email    string `gorm:"unique;not null"`
-	Password string
+	Name     string `gorm:"size:255"`
+	Email    string `gorm:"unique;not null;size:255"`
+	Password string `gorm:"size:255"`
 }

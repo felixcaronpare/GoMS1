467bd6cba9a3520d799e992cf9827bee7f6c40d3
felixcp <145021906+felixcaronpare@users.noreply.github.com>
2025-01-15 22:19:43 -0500

    WIP: expanding CRUD code

--- pkg/v1/interfaces.go
+++ pkg/v1/interfaces.go
@@ -5,10 +5,16 @@ import "github.com/felixcaronpare/GoMS1/internal/models"
 type RepoInterface interface {
 
 	Create(models.Account) (models.Account, error)
+	Get(id string) (models.Account, error)
+	Update(models.Account) (error)
+	Delete(id string) (error)
 
 	GetByEmail(email string) (models.Account, error)
 }
 
 type UsecaseInterface interface {
 	Create(models.Account) (models.Account, error)
+    Get(id string) (models.Account, error)
+    Update(models.Account) (error)
+    Delete(id string) (error)
 }
--- pkg/v1/repository/gorm.go
+++ pkg/v1/repository/gorm.go
@@ -15,11 +15,38 @@ func New(db *gorm.DB) interfaces.RepoInterface {
 }
 
 //=========== CRUD ===========
-
+// Create
 func (repo *Repo) Create(account models.Account) (models.Account, error) {
 	err := repo.db.Create(&account).Error
 
 	return account, err
+}
+
+// Get
+func (repo *Repo) Get(id string) (models.Account, error){
+	var account models.Account
+	err := repo.db.Where("id = ?", id).First(&account).Error
+  
+	return account, err
+  }
+  
+// Update
+func (repo *Repo) Update(account models.Account) error{
+var dbAccount models.Account
+if err := repo.db.Where("id = ?", account.ID).First(&dbAccount).Error; err != nil {
+	return err
+}
+dbAccount.Name = account.Name
+err := repo.db.Save(dbAccount).Error
+
+return err
+}
+
+// Delete
+func (repo *Repo) Delete(id string) error{
+err := repo.db.Where("id = ?", id).Delete(&models.Account{}).Error
+
+return err
 }
 
 //=========== UTILS ===========
